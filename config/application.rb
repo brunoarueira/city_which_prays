7415ae2c5f922a7d3e8b80cb41b507dfcb8601a8
Bruno Arueira <contato@brunoarueira.com>
2016-10-31 19:31:41 -0200

    Fixes the autoload paths

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module CityWhichPrays
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W(
+      #{config.root}/lib
+      #{config.root}/app/business
+    )
   end
 end
--- config/initializers/autoload_paths.rb
+++
@@ -1,6 +0,0 @@
-config = Rails.application.config
-
-config.autoload_paths += %W(
-  #{config.root}/lib
-  #{config.root}/business
-)
